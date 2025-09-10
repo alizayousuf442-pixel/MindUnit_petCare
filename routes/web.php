f5373db9aad92a9c4dcce3c0d9995d480a41c9b9
alizayousuf442-pixel <alizayousuf442@gmail.com>
2025-09-10 22:57:11 +0500

    new changing

--- routes/web.php
+++ routes/web.php
@@ -15,24 +15,26 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::prefix('/')->group(function() {
 
-Route::get('/', fn() => view('user.index'))->name('index');
-Route::get('/about', fn() => view('user.about'))->name('about');
-Route::get('/vet', fn() => view('user.vet'))->name('vet');
-Route::get('/services', fn() => view('user.services'))->name('services');
-Route::get('/gallery', fn() => view('user.gallery'))->name('gallery');
-Route::get('/shop', fn() => view('user.shop'))->name('shop');
-Route::get('/blog', fn() => view('user.blog'))->name('blog');
-Route::get('/contact', fn() => view('user.contact'))->name('contact');
-Route::get('/adopt', fn() => view('user.adopt'))->name('adopt');
-Route::get('/login', fn() => view('user.login'))->name('login');
-Route::get('/regis', fn() => view('user.registation'))->name('/regis');
-Route::get('/veterinerians', fn() => view('Veterinarians.vaccine.dashboard'))->name('/regis');
+Route::prefix('/')->group(function () {
 
+    Route::get('/', fn() => view('user.index'))->name('index');
+    Route::get('/about', fn() => view('user.about'))->name('about');
+    Route::get('/vet', fn() => view('user.vet'))->name('vet');
+    Route::get('/services', fn() => view('user.services'))->name('services');
+    Route::get('/gallery', fn() => view('user.gallery'))->name('gallery');
+    Route::get('/shop', fn() => view('user.shop'))->name('shop');
+    Route::get('/blog', fn() => view('user.blog'))->name('blog');
+    Route::get('/contact', fn() => view('user.contact'))->name('contact');
+    Route::get('/adopt', fn() => view('user.adopt'))->name('adopt');
 
+   
+    Route::get('/login', fn() => view('user.login'))->name('login');
+    Route::get('/regis', fn() => view('user.registation'))->name('regis');
 
-
-
+  
+    Route::get('/veterinarians', fn() => view('Veterinarians.vaccine.dashboard'))
+         ->name('veterinarians.dashboard');
 });
+
 
